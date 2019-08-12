0210b0a1a5206643346d091f11d71e08860efb0d
Golden <674897315@qq.com>
2019-08-12 17:57:07 +0800

    'Vue+Node课程第一课Demo'

---
+++ P7-Vue+node项目/Day1/ls-demo/src/components/Template.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <p>{{msg}}</p>
+        <!--强制绑定数据-->
+        <a v-bind:href="study">我真的棒</a>
+        <!--强制绑定响应事件-->
+        <button v-on:click="study">点我</button><br>
+        <button @click="study">点我</button><br>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Template",
+        data(){
+            return{
+                msg:'Golden How are you!'
+            }
+        },
+        methods:{
+            study(){
+                alert('你真的很棒!');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
